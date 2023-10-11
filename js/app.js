13d50e249ddc6a7179a80d417db01cedeb74796f
Mavlon4658 <mavlon4658@gmail.com>
2023-10-11 17:18:07 +0500

    main page responsive is ready

--- js/app.js
+++ js/app.js
@@ -1,18 +1,51 @@
 let full_page_bg = document.querySelector('.full_page_bg');
 let section_home = document.querySelector('.section_home');
 
-let card_slider = new Swiper(".card_slider", {
-    slidesPerView: 5,
-    spaceBetween: 13,
-    loop: true,
-});
+$(document).ready(function () {
+    let card_slider = new Swiper(".card_slider", {
+        slidesPerView: 1,
+        spaceBetween: 13,
+        breakpoints: {
+            1700: {
+                slidesPerView: 5,
+            },
+            768: {
+                slidesPerView: 3
+            },
+            500: {
+                slidesPerView: 2
+            }
+        },
+        loop: true,
+        navigation: {
+            nextEl: '.cards .left_btn'
+        }
+    });
+})
 
 let dealer_slider = new Swiper(".dealer_slider", {
-    slidesPerView: 3,
-    spaceBetween: 20,
+    slidesPerView: 1,
+    spaceBetween: 10,
     navigation: {
         nextEl: ".dealers_right",
         prevEl: ".dealers_left",
+    },
+    breakpoints: {
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        650: {
+            slidesPerView: 2
+        }
+    }
+});
+
+let partners_slider = new Swiper(".partners_slider", {
+    spaceBetween: 0,
+    navigation: {
+        nextEl: ".partners .right_btn",
+        prevEl: ".partners .left_btn",
     },
 });
 
@@ -28,3 +61,13 @@ new fullpage('#fullpage', {
         }
     }
 });
+
+$('#header .bars').click(function () {
+    $('.mobile_menues').removeClass('noActive');
+    $('.mobile_menues').addClass('active');
+})
+
+$('.mobile_menues .mobile_menu_close').click(function () {
+    $('.mobile_menues').removeClass('active');
+    $('.mobile_menues').addClass('noActive');
+})
